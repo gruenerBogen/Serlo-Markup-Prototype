2f774851e0bae22331848247bdad588bbc7ec5eb
Lukas <lukas.b@mailbox.org>
2021-05-23 12:00:47 +0200

    Refactor SerloModel JSON conversion
    Seperate the conversion from and to JSON into different files such that it is
    clearer which functions provide which functionality.

--- src/Serlo/Model/FromJSON.hs
+++ src/Serlo/Model/FromJSON.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-module SerloJSON where
+module Serlo.Model.FromJSON () where
 
 import Data.Aeson
 import Data.Aeson.Types (Parser)
@@ -7,32 +7,10 @@ import Data.Aeson.Types (Parser)
 import Data.Text (Text, unpack)
 import Text.Read (readMaybe)
 
-import qualified Data.String as S
-import qualified Data.Vector as V
-
-import Serlo.Model
-
-instance ToJSON SerloPlugin where
-  toJSON = pluginToJSON
+import Serlo.Internal.Model (SerloPlugin(..))
 
 instance FromJSON SerloPlugin where
   parseJSON = pluginFromJSON
-
-pluginToJSON :: SerloPlugin -> Value
-pluginToJSON p = object ["plugin" .= (name p), "state" .= (state p)]
-
-state :: SerloPlugin -> Value
-state (RichText t) = toJSONList t
-state (Rows ps) = rowsToJSON ps
-state (Spoiler title content) = spoilerToJSON title content
-state _ = String "test"
-
-rowsToJSON :: [SerloPlugin] -> Value
-rowsToJSON = Array . V.fromList . map pluginToJSON
-
-spoilerToJSON :: String -> SerloPlugin -> Value
-spoilerToJSON title content = object ["title" .= title, "content" .= pluginToJSON content]
-
 
 pluginFromJSON :: Value -> Parser SerloPlugin
 pluginFromJSON = withObject "SerloPlugin" $ \v -> do
@@ -48,7 +26,7 @@ pluginFromJSONConstructor name =
     "spoiler" -> parseSpoiler
     "rows" -> parseRows
     "injection" -> parseInjection
-    _ -> \_ -> fail $ "Plguin '" ++ (unpack name) ++ "' not implemented."
+    n -> \_ -> fail $ "Plguin '" ++ n ++ "' not implemented."
 
 parseImage :: Value -> Parser SerloPlugin
 parseImage = undefined
---
+++ src/Serlo/Model/ToJSON.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Serlo.Model.ToJSON () where
+
+import Data.Aeson
+import Data.Aeson.Types (Parser)
+
+import qualified Data.Vector as V
+
+import Serlo.Internal.Model (SerloPlugin(..))
+import Serlo.Model (name)
+
+instance ToJSON SerloPlugin where
+  toJSON = pluginToJSON
+
+pluginToJSON :: SerloPlugin -> Value
+pluginToJSON p = object ["plugin" .= (name p), "state" .= (state p)]
+
+state :: SerloPlugin -> Value
+state (RichText t) = toJSONList t
+state (Rows ps) = rowsToJSON ps
+state (Spoiler title content) = spoilerToJSON title content
+state _ = String "test"
+
+rowsToJSON :: [SerloPlugin] -> Value
+rowsToJSON = Array . V.fromList . map pluginToJSON
+
+spoilerToJSON :: String -> SerloPlugin -> Value
+spoilerToJSON title content = object ["title" .= title, "content" .= pluginToJSON content]
