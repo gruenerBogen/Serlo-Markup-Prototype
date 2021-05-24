dc0f274a6d2b55a61bda556e69d70e55389ad11a
Lukas <lukas.b@mailbox.org>
2021-05-24 18:02:57 +0200

    Add types to represent AsciiDoc paragraph content
    Most macros are not included right now.

---
+++ src/Text/AsciiDoc/Types/Text.hs
@@ -0,0 +1,24 @@
+module Text.AsciiDoc.Types.Text where
+
+import Text.AsciiDoc.Types.Generic (Attributes)
+
+type FormattedText = [FormattedSegment]
+
+data FormattedSegment = FormattedSegment Content [Formatting]
+                      deriving (Eq, Show)
+
+data Content = Text String
+             | Link Target Name
+             | Math String
+             deriving (Eq, Show)
+
+type Name = FormattedText
+type Target = String
+
+data Formatting = Bold
+                | Italic
+                | Monospace
+                | Highlight
+                | Subscript
+                | Superscript
+                deriving (Eq, Show)
