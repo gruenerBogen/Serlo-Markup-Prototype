7aa2799672319b2d3bd9abe92be32e8d06c046d2
Lukas <lukas.b@mailbox.org>
2021-08-04 17:41:46 +0200

    Properly encode block separators
    Previously blocks were not enclosed in any way - even though they should have
    to create a valid AsciiDoc.  Add open block separators around all generic
    blocks which do not have simple content.
    
    Correct context encoding is still missing.

--- src/Text/AsciiDoc/Encode/Block.hs
+++ src/Text/AsciiDoc/Encode/Block.hs
@@ -4,6 +4,12 @@ module Text.AsciiDoc.Encode.Block
   , encodeBlocks
   ) where
 
+import Control.Monad.Trans.State ( State
+                                 , evalState
+                                 , get
+                                 , put
+                                 )
+
 import Text.Read (readMaybe)
 import Text.PrettyPrint.HughesPJ hiding ((<>))
 
@@ -16,32 +22,81 @@ import Text.AsciiDoc.Types.Text (FormattedText)
 import Text.AsciiDoc.Encode.Text (encodeFormattedText)
 
 encodeBlock :: Block -> Doc
-encodeBlock b = maybeEncodeAttributes (attributes b)
-             <> case context b of
-                 "section" -> encodeAsSection b
-                 _ -> encodeGenericBlock b
+encodeBlock = (flip evalState) initState . blockEncoder
 
-encodeAsSection :: Block -> Doc
-encodeAsSection b = let level = (maybe 0 id $ readMaybe =<< (lookupNamed "level" $ attributes b))
-                        t = maybe [] id (title b)
-                    in text (take (level + 1) $ repeat '=')
-                    <> char ' '
-                    <> encodeFormattedText t
-                    -- This \n should not be part of the title, but currently
-                    -- it is. Hence it is currently commented out.
-                    -- <> char '\n'
-                    <> encodeContent (content b)
+encodeBlocks :: [Block] -> Doc
+encodeBlocks = (flip evalState) initState . blocksEncoder
 
--- TODO: Do something with the context
-encodeGenericBlock :: Block -> Doc
-encodeGenericBlock b = encodeBlockTitle (title b)
-                    <> encodeContent (content b)
+--------------------------------------------------------------------------------
 
-encodeContent :: BlockContent -> Doc
-encodeContent (Compount bs) = encodeBlocks bs
-encodeContent (Simple ft) = encodeFormattedText ft
-encodeContent (Raw s) = text s
-encodeContent Empty = empty
+data EncoderState = EncoderState { depth :: Int
+                                 , sectionLevel :: Int
+                                 }
+
+initState :: EncoderState
+initState = EncoderState { depth = 0
+                         , sectionLevel = 0
+                         }
+
+type Encoder a = State EncoderState a
+
+type BlockEncoder = Block -> (Encoder Doc)
+
+blockEncoder :: BlockEncoder
+blockEncoder b =
+  case context b of
+    "section" -> sectionEncoder b
+    _ -> genericBlockEncoder b
+
+sectionEncoder :: BlockEncoder
+sectionEncoder b = do
+  s <- get
+  let level = (maybe ((sectionLevel s)+1) id $ readMaybe =<< (lookupNamed "level" $ attributes b))
+  let t = maybe [] id (title b)
+  put $ s { sectionLevel = level }
+  c <- contentEncoder empty b
+  put s
+  return $ text (take (level + 1) $ repeat '=')
+        <> char ' '
+    <> encodeFormattedText t
+    -- This \n should not be part of the title, but currently
+    -- it is. Hence it is currently commented out.
+    -- <> char '\n'
+    <> c
+
+genericBlockEncoder :: BlockEncoder
+genericBlockEncoder b = do
+  s <- get
+  let d = depth s
+  let sep = take (d+2) $ repeat '-'
+  let sepDoc = if (null sep)
+               then empty
+               else (text sep)
+  put s { depth = (d+2) }
+  c <- contentEncoder sepDoc b
+  put s
+  return $ encodeBlockTitle (title b)
+        <> maybeEncodeAttributes (attributes b)
+        <> c
+
+blocksEncoder :: [Block] -> Encoder Doc
+blocksEncoder = fmap (hcat . punctuate (text "\n\n")) . mapM blockEncoder
+
+-- TODO Ensure that the contentEncoder produces Docs which end with a new line
+contentEncoder :: Doc -> Block -> Encoder Doc
+contentEncoder sep b =
+  case content b of
+    Compount bs -> applySeparator sep <$> blocksEncoder bs
+    Simple t -> return (encodeFormattedText t)
+    Raw s -> return $ applySeparator sep (text s)
+    Empty -> return empty
+
+--------------------------------------------------------------------------------
+
+applySeparator :: Doc -> Doc -> Doc
+applySeparator s
+  | isEmpty s = id
+  | otherwise = sourround (s <> char '\n') (char '\n' <> s)
 
 maybeEncodeAttributes :: Attributes -> Doc
 maybeEncodeAttributes a = if (attributesEmpty $ a)
@@ -54,9 +109,6 @@ encodeAttributes (Attributes pos named) = brackets $ hcat $ punctuate (char ',')
 
 encodeBlockTitle :: Maybe FormattedText -> Doc
 encodeBlockTitle = maybe empty (sourround (char '.') (char '\n') . encodeFormattedText)
-
-encodeBlocks :: [Block] -> Doc
-encodeBlocks = hcat . punctuate (text "\n\n") . map encodeBlock
 
 sourround :: Doc -> Doc -> Doc -> Doc
 sourround start end mid = start <> mid <> end
